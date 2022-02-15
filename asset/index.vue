d5b017fa58f5e3c7cea647d1e850010931967038
adamdediev <80171494+adamdediev@users.noreply.github.com>
2022-02-15 16:43:00 +0300

    Add files via upload

---
+++ asset/index.vue
@@ -0,0 +1,18 @@
+Vue.component('component-a', {
+	template: '<div class="input_box"><div class="input_name input"><span>Имя</span><input type="text"></div><div class="input_old_years input"><span>Возраст</span><input type="text"></div><div class="delete"><span>Удалить</span></div></div>'
+})
+
+new Vue({
+  el: "#app",
+  data: {
+  	arr: []
+    },
+    methods: {
+    	add(){
+      	this.arr.push({
+          
+        });
+        
+      }
+    }
+})
